1df3657d3f4ac7aa744b9092fb4b31aa85f40d39
Alex <cptmutchell@gmail.com>
2022-09-26 22:20:47 +0300

    scss

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
-import './scss/AboutPage.css';
+import './scss/AboutPage.scss';
 import logo from "./avatar.jpeg";
 
 
