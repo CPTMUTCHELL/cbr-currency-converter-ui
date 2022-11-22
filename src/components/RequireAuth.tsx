2e6234cebe8cf0193129491761174f89a3b55266
Alex <cptmutchell@gmail.com>
2022-11-22 15:01:58 +0300

    navigate to login page if no token

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from "react";
-import { Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import {UserContext} from "../functions/UserContext";
 import {getUser} from "../functions/JwtToken";
 import { singletonTokenInstance} from "../functions/Tokens";
@@ -10,6 +10,7 @@ export const RequireAuth:React.FC<{allowedRoles:string[]}> = ({allowedRoles}) =>
     const {userToken,setUserToken}  = useContext(UserContext);
     const token = singletonTokenInstance.getToken().access
     const [useEffectCompleted,setUseEffectCompleted] =useState(false);
+    const navigate = useNavigate();
     useEffect(()=>{
 
         if (token!==null) {
@@ -30,6 +31,7 @@ export const RequireAuth:React.FC<{allowedRoles:string[]}> = ({allowedRoles}) =>
             return <Outlet/>
 
     }
+    if (!token) navigate("/login")
     return <NotAuthorizedPage/>
 
 
