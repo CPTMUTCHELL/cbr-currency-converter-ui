e2ac2f2c0df6b317af0c2da12d621a2d5205d64b
Alex <cptmutchell@gmail.com>
2023-01-16 23:09:18 +0300

    pdf

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from "./avatar.jpeg";
 import './scss/AboutPage.scss';
-import resume from "./resume.pdf"
 
 export const AboutPage: React.FC = () => {
     return <>
@@ -11,7 +10,7 @@ export const AboutPage: React.FC = () => {
             <img src={logo} width="340" height="340"/>
                 </div>
 
-            <a style={{textDecoration:"none"}} href={resume} download="resume.pdf">
+            <a style={{textDecoration:"none"}} href="build/88cbc24eb97dc1b1215e.pdf" download="resume.pdf">
                 <div className="download__btn">
                     <span>
                     Download CV (PDF)</span>
